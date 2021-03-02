4fdff1a6d8d11c68d89b32c4e9709eb1c1b4789a
Tom Larsen <larsenthomasj@gmail.com>
2021-03-02 09:58:37 -0500

    add additional selectors

--- flutter/input/lib/path_selector.dart
+++ flutter/input/lib/path_selector.dart
@@ -20,8 +20,30 @@ class PathSelector extends StatelessWidget {
         switch (lastElement.DataType){
           case 'Path':
             return SelectPathChild(field);
+          case 'List:Node':
+          case 'List:Path':
+          case 'List:Relationship':
+          case 'List:Integer':
+          case 'List:Float':
+          case 'List:DateTime':
+          case 'List:Boolean':
+          case 'List:String':
+            var itemType = lastElement.DataType.split(':')[1];
+            return SelectListChild(field, itemType);
+          case 'Node':
+            return SelectNodeChild(field);
+          case 'Relationship':
+            return SelectRelationshipChild(field);
+          case 'Map':
+            return SelectMapChild(field);
+          case 'Integer':
+          case 'Float':
+          case 'DateTime':
+          case 'Boolean':
+          case 'String':
+            return SizedBox(height: 0);
           default:
-            return Text("hello world");
+            return Text("The current path ends in an invalid data type");
         }
       },
     );
@@ -72,6 +94,117 @@ class SelectPathChild extends StatelessWidget {
         var fieldState = BlocProvider.of<FieldState>(context);
         fieldState.addElementToPath(c.Element(Key: e.name, DataType: e.dataType));
       },
+    );
+  }
+}
+
+class SelectListChild extends StatelessWidget {
+  SelectListChild(this.field, this.itemType);
+  final c.Field field;
+  final String itemType;
+
+  Widget build(BuildContext context) {
+    return DropdownButton<SelectData>(
+      items: [
+        DropdownMenuItem<SelectData>(child: Text("First"), value: SelectData("First", itemType)),
+        DropdownMenuItem<SelectData>(child: Text("Last"), value: SelectData("Last", itemType)),
+      ],
+      onChanged: (e){
+        var fieldState = BlocProvider.of<FieldState>(context);
+        fieldState.addElementToPath(c.Element(Key: e.name, DataType: e.dataType));
+      },
+    );
+  }
+}
+
+class SelectNodeChild extends StatelessWidget {
+  SelectNodeChild(this.field);
+  final c.Field field;
+
+  Widget build(BuildContext context) {
+    return DropdownButton<SelectData>(
+      items: [
+        DropdownMenuItem<SelectData>(child: Text("ID"), value: SelectData("ID", 'Integer')),
+        DropdownMenuItem<SelectData>(child: Text("Labels"), value: SelectData("Labels", 'List:String')),
+        DropdownMenuItem<SelectData>(child: Text("Properties"), value: SelectData("Properties", 'Map')),
+      ],
+      onChanged: (e){
+        var fieldState = BlocProvider.of<FieldState>(context);
+        fieldState.addElementToPath(c.Element(Key: e.name, DataType: e.dataType));
+      },
+    );
+  }
+}
+
+class SelectRelationshipChild extends StatelessWidget {
+  SelectRelationshipChild(this.field);
+  final c.Field field;
+
+  Widget build(BuildContext context) {
+    return DropdownButton<SelectData>(
+      items: [
+        DropdownMenuItem<SelectData>(child: Text("ID"), value: SelectData("ID", 'Integer')),
+        DropdownMenuItem<SelectData>(child: Text("StartId"), value: SelectData("StartId", 'Integer')),
+        DropdownMenuItem<SelectData>(child: Text("EndId"), value: SelectData("EndId", 'Integer')),
+        DropdownMenuItem<SelectData>(child: Text("Type"), value: SelectData("Type", 'String')),
+        DropdownMenuItem<SelectData>(child: Text("Properties"), value: SelectData("Properties", 'Map')),
+      ],
+      onChanged: (e){
+        var fieldState = BlocProvider.of<FieldState>(context);
+        fieldState.addElementToPath(c.Element(Key: e.name, DataType: e.dataType));
+      },
+    );
+  }
+}
+
+class SelectMapChild extends StatefulWidget {
+  SelectMapChild(this.field);
+  final c.Field field;
+
+  State<StatefulWidget> createState() => _SelectMapChildState();
+}
+
+class _SelectMapChildState extends State<SelectMapChild>{
+
+  TextEditingController _property = TextEditingController(text: '');
+  String _selectedType;
+
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        SizedBox(
+          width: 200,
+          child: TextField(
+            decoration: InputDecoration(labelText: 'Property name'),
+            controller: _property,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+          child: DropdownButton<String>(
+            hint: Text("Select property type"),
+            value: _selectedType,
+            items: [
+              DropdownMenuItem<String>(child: Text("Boolean"), value: "Boolean"),
+              DropdownMenuItem<String>(child: Text("DateTime"), value: "DateTime"),
+              DropdownMenuItem<String>(child: Text("Float"), value: "Float"),
+              DropdownMenuItem<String>(child: Text("Integer"), value: "Integer"),
+              DropdownMenuItem<String>(child: Text("String"), value: "String"),
+            ],
+            onChanged: (e){
+              setState(() =>_selectedType = e);
+            },
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.chevron_right),
+          onPressed: (){
+            var fieldState = BlocProvider.of<FieldState>(context);
+            fieldState.addElementToPath(c.Element(Key: _property.text, DataType: _selectedType));
+          },
+        ),
+      ],
     );
   }
 }
