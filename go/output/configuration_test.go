701e9db18c1ceb2dd3c5babbc2158cbdda1ebb29
Tom <larsenthomasj@gmail.com>
2021-04-15 07:44:12 -0400

    build node output query that has no IDs

--- go/output/configuration_test.go
+++ go/output/configuration_test.go
@@ -12,7 +12,7 @@ type ConfigStruct struct {
 	PropFields []string
 }
 
-func buildMergeClause(builder *strings.Builder, config *ConfigStruct) {
+func mergeNodeClause(builder *strings.Builder, config *ConfigStruct) {
 	builder.WriteString(fmt.Sprintf("MERGE (newNode:`%v`{", config.Label))
 	for index, id := range config.IdFields {
 		if index > 0 {
@@ -21,6 +21,17 @@ func buildMergeClause(builder *strings.Builder, config *ConfigStruct) {
 		builder.WriteString(fmt.Sprintf("`%v`:row.`%v`", id, id))
 	}
 	builder.WriteString("})\n")
+}
+
+func createNodeClause(builder *strings.Builder, config *ConfigStruct) {
+	builder.WriteString(fmt.Sprintf("CREATE (newNode:`%v`{", config.Label))
+	for index, id := range config.PropFields {
+		if index > 0 {
+			builder.WriteString(",")
+		}
+		builder.WriteString(fmt.Sprintf("`%v`:row.`%v`", id, id))
+	}
+	builder.WriteString("})")
 }
 
 func buildSetProperties(builder *strings.Builder, config *ConfigStruct) {
@@ -35,7 +46,11 @@ func buildSetProperties(builder *strings.Builder, config *ConfigStruct) {
 func NodeQuery(config *ConfigStruct) string {
 	builder := &strings.Builder{}
 	builder.WriteString("UNWIND $batch AS row\n")
-	buildMergeClause(builder, config)
+	if len(config.IdFields) == 0 {
+		createNodeClause(builder, config)
+		return builder.String()
+	}
+	mergeNodeClause(builder, config)
 	if len(config.PropFields) == 0 {
 		return builder.String()
 	}
@@ -74,6 +89,21 @@ func TestGenerateNodeQueryWithNoProperties(t *testing.T) {
 	query := NodeQuery(config)
 	expected := "UNWIND $batch AS row\n" +
 		"MERGE (newNode:`TestLabel`{`id1`:row.`id1`,`id2`:row.`id2`})\n"
+
+	if expected != query {
+		t.Fatalf("expected\n\n%v\n\nbut got\n\n%v", expected, query)
+	}
+}
+
+func TestGenerateNodesWithNoIds(t *testing.T) {
+	config := &ConfigStruct{
+		Label:      `TestLabel`,
+		IdFields:   nil,
+		PropFields: []string{`prop1`, `prop2`},
+	}
+	query := NodeQuery(config)
+	expected := "UNWIND $batch AS row\n" +
+		"CREATE (newNode:`TestLabel`{`prop1`:row.`prop1`,`prop2`:row.`prop2`})"
 
 	if expected != query {
 		t.Fatalf("expected\n\n%v\n\nbut got\n\n%v", expected, query)
