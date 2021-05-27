19e523ae3809c30644fbbe7d7872ad825c21c51d
Tom Larsen <larsenthomasj@gmail.com>
2021-05-27 13:28:09 -0400

    finish generating output query

--- go/delete/configuration.go
+++ go/delete/configuration.go
@@ -1,6 +1,9 @@
 package delete
 
-import "strings"
+import (
+	"errors"
+	"strings"
+)
 
 type DeleteNodesProperties struct {
 	Label    string
@@ -18,20 +21,14 @@ func GenerateDeleteNodes(props *DeleteNodesProperties) string {
 	props.escape()
 	builder := &strings.Builder{}
 	builder.WriteString("UNWIND $batch AS row\n")
-	builder.WriteString("MATCH (d:`")
-	builder.WriteString(props.Label)
-	builder.WriteString("`{")
-	for index, idField := range props.IdFields {
-		if index > 0 {
-			builder.WriteByte(',')
-		}
-		builder.WriteByte('`')
-		builder.WriteString(idField)
-		builder.WriteString("`:row.`")
-		builder.WriteString(idField)
-		builder.WriteByte('`')
+	builder.WriteString("MATCH (d")
+	if props.Label != `` {
+		writeLabel(builder, props.Label)
 	}
-	builder.WriteString("}) DETACH DELETE d")
+	if len(props.IdFields) > 0 {
+		writeProperties(builder, props.IdFields, props.IdFields)
+	}
+	builder.WriteString(") DETACH DELETE d")
 	return builder.String()
 }
 
@@ -46,13 +43,74 @@ type DeleteRelationshipsProperties struct {
 	RightNodeNeo4jFields   []string
 }
 
-func GenerateDeleteRelationships(props DeleteRelationshipsProperties) string {
+func (p *DeleteRelationshipsProperties) escape() {
+	p.RelType = escapeName(p.RelType)
+	p.LeftNodeLabel = escapeName(p.LeftNodeLabel)
+	p.RightNodeLabel = escapeName(p.RightNodeLabel)
+	for _, strList := range [][]string{p.RelFields, p.LeftNodeAlteryxFields, p.LeftNodeNeo4jFields, p.RightNodeAlteryxFields, p.RightNodeNeo4jFields} {
+		for index, item := range strList {
+			strList[index] = escapeName(item)
+		}
+	}
+}
+
+func GenerateDeleteRelationships(props DeleteRelationshipsProperties) (string, error) {
+	if len(props.LeftNodeAlteryxFields) != len(props.LeftNodeNeo4jFields) {
+		return ``, errors.New(`the number of left node Alteryx fields does not match the number of left node Neo4j fields`)
+	}
+	if len(props.RightNodeAlteryxFields) != len(props.RightNodeNeo4jFields) {
+		return ``, errors.New(`the number of right node Alteryx fields does not match the number of right node Neo4j fields`)
+	}
+
+	props.escape()
 	builder := &strings.Builder{}
 	builder.WriteString("UNWIND $batch AS row\n")
-	builder.WriteString("MATCH (:`Customer` {`Key`:row.`LeftKey`})-[r:`IS_RELATED`]-(:`Customer` {`Key`:row.`RightKey`}) DELETE r")
-	return builder.String()
+	builder.WriteString("MATCH (")
+	if props.LeftNodeLabel != `` {
+		writeLabel(builder, props.LeftNodeLabel)
+	}
+	if len(props.LeftNodeNeo4jFields) > 0 {
+		writeProperties(builder, props.LeftNodeNeo4jFields, props.LeftNodeAlteryxFields)
+	}
+	builder.WriteString(")-[r")
+	if props.RelType != `` {
+		writeLabel(builder, props.RelType)
+	}
+	if len(props.RelFields) > 0 {
+		writeProperties(builder, props.RelFields, props.RelFields)
+	}
+	builder.WriteString("]-(")
+	if props.RightNodeLabel != `` {
+		writeLabel(builder, props.RightNodeLabel)
+	}
+	if len(props.RightNodeNeo4jFields) > 0 {
+		writeProperties(builder, props.RightNodeNeo4jFields, props.RightNodeAlteryxFields)
+	}
+	builder.WriteString(") DELETE r")
+	return builder.String(), nil
 }
 
 func escapeName(name string) string {
 	return strings.Replace(name, "`", "``", -1)
+}
+
+func writeLabel(builder *strings.Builder, label string) {
+	builder.WriteString(":`")
+	builder.WriteString(label)
+	builder.WriteByte('`')
+}
+
+func writeProperties(builder *strings.Builder, neo4jFields []string, alteryxFields []string) {
+	builder.WriteString(" {")
+	for index, neo4jKey := range neo4jFields {
+		if index > 0 {
+			builder.WriteByte(',')
+		}
+		builder.WriteByte('`')
+		builder.WriteString(neo4jKey)
+		builder.WriteString("`:row.`")
+		builder.WriteString(alteryxFields[index])
+		builder.WriteByte('`')
+	}
+	builder.WriteByte('}')
 }
--- go/delete/configuration_test.go
+++ go/delete/configuration_test.go
@@ -11,7 +11,7 @@ func TestDeleteNode(t *testing.T) {
 
 	query := delete.GenerateDeleteNodes(props)
 	expected := "UNWIND $batch AS row\n" +
-		"MATCH (d:`Customer`{`Key`:row.`Key`}) DETACH DELETE d"
+		"MATCH (d:`Customer` {`Key`:row.`Key`}) DETACH DELETE d"
 
 	if query != expected {
 		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
@@ -26,7 +26,7 @@ func TestDeleteNodeUsingMultipleProperties(t *testing.T) {
 
 	query := delete.GenerateDeleteNodes(props)
 	expected := "UNWIND $batch AS row\n" +
-		"MATCH (d:`Customer`{`Key1`:row.`Key1`,`Key2`:row.`Key2`}) DETACH DELETE d"
+		"MATCH (d:`Customer` {`Key1`:row.`Key1`,`Key2`:row.`Key2`}) DETACH DELETE d"
 
 	if query != expected {
 		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
@@ -41,7 +41,35 @@ func TestDeleteNodeWithBackticks(t *testing.T) {
 
 	query := delete.GenerateDeleteNodes(props)
 	expected := "UNWIND $batch AS row\n" +
-		"MATCH (d:`Cust``omer`{`Ke``y`:row.`Ke``y`}) DETACH DELETE d"
+		"MATCH (d:`Cust``omer` {`Ke``y`:row.`Ke``y`}) DETACH DELETE d"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteNodeWithoutIdFields(t *testing.T) {
+	props := &delete.DeleteNodesProperties{
+		Label: `Customer`,
+	}
+
+	query := delete.GenerateDeleteNodes(props)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH (d:`Customer`) DETACH DELETE d"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteNodeWithoutLabel(t *testing.T) {
+	props := &delete.DeleteNodesProperties{
+		IdFields: []string{`Key`},
+	}
+
+	query := delete.GenerateDeleteNodes(props)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH (d {`Key`:row.`Key`}) DETACH DELETE d"
 
 	if query != expected {
 		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
@@ -60,11 +88,192 @@ func TestDeleteRelationship(t *testing.T) {
 		RightNodeNeo4jFields:   []string{`Key`},
 	}
 
-	query := delete.GenerateDeleteRelationships(props)
+	query, _ := delete.GenerateDeleteRelationships(props)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH (:`Customer` {`Key`:row.`LeftKey`})-[r:`IS_RELATED` {`Prop`:row.`Prop`}]-(:`Customer` {`Key`:row.`RightKey`}) DELETE r"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteRelationshipsWithBackticks(t *testing.T) {
+	props := delete.DeleteRelationshipsProperties{
+		RelType:                "IS_`RELATED",
+		RelFields:              []string{"Pro`p"},
+		LeftNodeLabel:          "Cust`omer",
+		LeftNodeAlteryxFields:  []string{"LeftKe`y"},
+		LeftNodeNeo4jFields:    []string{"Ke`y"},
+		RightNodeLabel:         "Cust`omer",
+		RightNodeAlteryxFields: []string{"RightKe`y"},
+		RightNodeNeo4jFields:   []string{"Ke`y"},
+	}
+
+	query, _ := delete.GenerateDeleteRelationships(props)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH (:`Cust``omer` {`Ke``y`:row.`LeftKe``y`})-[r:`IS_``RELATED` {`Pro``p`:row.`Pro``p`}]-(:`Cust``omer` {`Ke``y`:row.`RightKe``y`}) DELETE r"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteRelationshipWithoutLeftFields(t *testing.T) {
+	props := delete.DeleteRelationshipsProperties{
+		RelType:                `IS_RELATED`,
+		RelFields:              []string{`Prop`},
+		LeftNodeLabel:          `Customer`,
+		RightNodeLabel:         `Customer`,
+		RightNodeAlteryxFields: []string{`RightKey`},
+		RightNodeNeo4jFields:   []string{`Key`},
+	}
+
+	query, _ := delete.GenerateDeleteRelationships(props)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH (:`Customer`)-[r:`IS_RELATED` {`Prop`:row.`Prop`}]-(:`Customer` {`Key`:row.`RightKey`}) DELETE r"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteRelationshipWithoutRightFields(t *testing.T) {
+	props := delete.DeleteRelationshipsProperties{
+		RelType:               `IS_RELATED`,
+		RelFields:             []string{`Prop`},
+		LeftNodeLabel:         `Customer`,
+		LeftNodeAlteryxFields: []string{"LeftKey"},
+		LeftNodeNeo4jFields:   []string{"Key"},
+		RightNodeLabel:        `Customer`,
+	}
+
+	query, _ := delete.GenerateDeleteRelationships(props)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH (:`Customer` {`Key`:row.`LeftKey`})-[r:`IS_RELATED` {`Prop`:row.`Prop`}]-(:`Customer`) DELETE r"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteRelationshipWithoutRelationshipFields(t *testing.T) {
+	props := delete.DeleteRelationshipsProperties{
+		RelType:                `IS_RELATED`,
+		LeftNodeLabel:          `Customer`,
+		LeftNodeAlteryxFields:  []string{`LeftKey`},
+		LeftNodeNeo4jFields:    []string{`Key`},
+		RightNodeLabel:         `Customer`,
+		RightNodeAlteryxFields: []string{`RightKey`},
+		RightNodeNeo4jFields:   []string{`Key`},
+	}
+
+	query, _ := delete.GenerateDeleteRelationships(props)
 	expected := "UNWIND $batch AS row\n" +
 		"MATCH (:`Customer` {`Key`:row.`LeftKey`})-[r:`IS_RELATED`]-(:`Customer` {`Key`:row.`RightKey`}) DELETE r"
 
 	if query != expected {
 		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteRelationshipWithoutLeftLabel(t *testing.T) {
+	props := delete.DeleteRelationshipsProperties{
+		RelType:                `IS_RELATED`,
+		RelFields:              []string{`Prop`},
+		LeftNodeAlteryxFields:  []string{`LeftKey`},
+		LeftNodeNeo4jFields:    []string{`Key`},
+		RightNodeLabel:         `Customer`,
+		RightNodeAlteryxFields: []string{`RightKey`},
+		RightNodeNeo4jFields:   []string{`Key`},
+	}
+
+	query, _ := delete.GenerateDeleteRelationships(props)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH ( {`Key`:row.`LeftKey`})-[r:`IS_RELATED` {`Prop`:row.`Prop`}]-(:`Customer` {`Key`:row.`RightKey`}) DELETE r"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteRelationshipWithoutRelationshipType(t *testing.T) {
+	props := delete.DeleteRelationshipsProperties{
+		RelFields:              []string{`Prop`},
+		LeftNodeLabel:          `Customer`,
+		LeftNodeAlteryxFields:  []string{`LeftKey`},
+		LeftNodeNeo4jFields:    []string{`Key`},
+		RightNodeLabel:         `Customer`,
+		RightNodeAlteryxFields: []string{`RightKey`},
+		RightNodeNeo4jFields:   []string{`Key`},
+	}
+
+	query, _ := delete.GenerateDeleteRelationships(props)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH (:`Customer` {`Key`:row.`LeftKey`})-[r {`Prop`:row.`Prop`}]-(:`Customer` {`Key`:row.`RightKey`}) DELETE r"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteRelationshipWithoutRightLabel(t *testing.T) {
+	props := delete.DeleteRelationshipsProperties{
+		RelType:                `IS_RELATED`,
+		RelFields:              []string{`Prop`},
+		LeftNodeLabel:          `Customer`,
+		LeftNodeAlteryxFields:  []string{`LeftKey`},
+		LeftNodeNeo4jFields:    []string{`Key`},
+		RightNodeAlteryxFields: []string{`RightKey`},
+		RightNodeNeo4jFields:   []string{`Key`},
+	}
+
+	query, _ := delete.GenerateDeleteRelationships(props)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH (:`Customer` {`Key`:row.`LeftKey`})-[r:`IS_RELATED` {`Prop`:row.`Prop`}]-( {`Key`:row.`RightKey`}) DELETE r"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteRelationshipMismatchedLeftFields(t *testing.T) {
+	props := delete.DeleteRelationshipsProperties{
+		RelType:                `IS_RELATED`,
+		RelFields:              []string{`Prop`},
+		LeftNodeLabel:          `Customer`,
+		LeftNodeAlteryxFields:  []string{`LeftKey`},
+		LeftNodeNeo4jFields:    []string{`Key1`, `Key2`},
+		RightNodeLabel:         `Customer`,
+		RightNodeAlteryxFields: []string{`RightKey`},
+		RightNodeNeo4jFields:   []string{`Key`},
+	}
+
+	query, err := delete.GenerateDeleteRelationships(props)
+	if err == nil {
+		t.Fatalf(`expected an error but got none`)
+	}
+	if query != `` {
+		t.Fatalf("expected empty string but got\n%v", query)
+	}
+}
+
+func TestDeleteRelationshipMismatchedRightFields(t *testing.T) {
+	props := delete.DeleteRelationshipsProperties{
+		RelType:                `IS_RELATED`,
+		RelFields:              []string{`Prop`},
+		LeftNodeLabel:          `Customer`,
+		LeftNodeAlteryxFields:  []string{`LeftKey`},
+		LeftNodeNeo4jFields:    []string{`Key`},
+		RightNodeLabel:         `Customer`,
+		RightNodeAlteryxFields: []string{`RightKey`},
+		RightNodeNeo4jFields:   []string{`Key1`, `Key2`},
+	}
+
+	query, err := delete.GenerateDeleteRelationships(props)
+	if err == nil {
+		t.Fatalf(`expected an error but got none`)
+	}
+	if query != `` {
+		t.Fatalf("expected empty string but got\n%v", query)
 	}
 }
