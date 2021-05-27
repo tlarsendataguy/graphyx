4201c263982494772e78fca52a8da11b816fcfff
Tom Larsen <larsenthomasj@gmail.com>
2021-05-27 06:06:38 -0400

    generate delete nodes query

--- go/delete/configuration.go
+++ go/delete/configuration.go
@@ -5,6 +5,24 @@ import "strings"
 func GenerateDeleteNodes(label string, idFields []string) string {
 	builder := &strings.Builder{}
 	builder.WriteString("UNWIND $batch AS row\n")
-	builder.WriteString("MATCH (d:`Customer`{`Key`:row.`Key`}) DETACH DELETE d")
+	builder.WriteString("MATCH (d:`")
+	builder.WriteString(escapeName(label))
+	builder.WriteString("`{")
+	for index, idField := range idFields {
+		if index > 0 {
+			builder.WriteByte(',')
+		}
+		escaped := escapeName(idField)
+		builder.WriteByte('`')
+		builder.WriteString(escaped)
+		builder.WriteString("`:row.`")
+		builder.WriteString(escaped)
+		builder.WriteByte('`')
+	}
+	builder.WriteString("}) DETACH DELETE d")
 	return builder.String()
+}
+
+func escapeName(name string) string {
+	return strings.Replace(name, "`", "``", -1)
 }
--- go/delete/configuration_test.go
+++ go/delete/configuration_test.go
@@ -15,3 +15,29 @@ func TestDeleteNode(t *testing.T) {
 		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
 	}
 }
+
+func TestDeleteNodeUsingMultipleProperties(t *testing.T) {
+	label := `Customer`
+	idFields := []string{`Key1`, `Key2`}
+
+	query := delete.GenerateDeleteNodes(label, idFields)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH (d:`Customer`{`Key1`:row.`Key1`,`Key2`:row.`Key2`}) DETACH DELETE d"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
+
+func TestDeleteNodeWithBackticks(t *testing.T) {
+	label := "Cust`omer"
+	idFields := []string{"Ke`y"}
+
+	query := delete.GenerateDeleteNodes(label, idFields)
+	expected := "UNWIND $batch AS row\n" +
+		"MATCH (d:`Cust``omer`{`Ke``y`:row.`Ke``y`}) DETACH DELETE d"
+
+	if query != expected {
+		t.Fatalf("expected\n%v\nbut got\n%v", expected, query)
+	}
+}
